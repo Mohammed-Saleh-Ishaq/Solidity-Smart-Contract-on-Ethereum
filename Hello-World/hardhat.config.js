91963a5688d3bc8ae6cece4b4f13061c906f9fd8
Mohammed-Saleh-Ishaq <mohammedishaq0592@gmail.com>
2024-04-28 05:20:09 +0530

    updated hardhat.config.js

--- Hello-World/hardhat.config.js
+++ Hello-World/hardhat.config.js
@@ -4,3 +4,20 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.24",
 };
+
+
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/98H2itVSqo-KrZYSQJV-zfu31EndjXQQ",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
